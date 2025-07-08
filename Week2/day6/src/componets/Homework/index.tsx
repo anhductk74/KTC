24f9c09922346d9eeb35be730ebb22b76a823ea1
anhductk74 <anhduc120102@gmail.com>
2025-07-08 09:50:16 +0700

    day6update

--- Week2/day6/src/componets/Homework/index.tsx
+++ Week2/day6/src/componets/Homework/index.tsx
@@ -6,9 +6,17 @@ const WeatherComponent = () => {
   const [location, setLocation] = useState<string>('Da Nang');
   const [weather, setWeather] = useState<any>(null);
   const [count, setCount] = useState<number>(0)
+  const currentTime = new Date().toLocaleTimeString('vi-VN', {
+    hour12: false, // Dạng 24h
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+
+  const [hourly, setHourly] = useState([])
 
   const handleChangeLocation = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLocation(e.target.value);
+    setLocation(e.target.value)
     // setCount(prev=>prev+1)
   };
 
@@ -18,10 +26,39 @@ const WeatherComponent = () => {
       setCount(prev=>prev+1)
     }
   }
+
+  const handleWeatherHourly = () => {
+  const forecastToday = weather?.forecast?.forecastday[0]?.hour || [];
+  const forecastTomorrow = weather?.forecast?.forecastday[1]?.hour || [];
+  const lastUpdateHour = parseInt(weather?.current?.last_updated?.split(' ')[1]?.split(':')[0] || "0");
+
+  // Lấy các giờ lớn hơn lastUpdate từ hôm nay
+  const futureToday = forecastToday.filter((item) => {
+    const hour = parseInt(item?.time?.split(' ')[1]?.split(':')[0]);
+    return hour > lastUpdateHour;
+  });
+
+  // Nếu đủ 4 giờ → dùng luôn
+  if (futureToday.length >= 4) {
+    return futureToday.slice(0, 4);
+  }
+
+  // Nếu không đủ, bù thêm từ forecast ngày mai
+  const needed = 4 - futureToday.length;
+  const futureTomorrow = forecastTomorrow.slice(0, needed);
+
+  return [...futureToday, ...futureTomorrow];
+};
+
+
+
+
+
+
   
   useEffect(() => {
     const fetchWeather = async () => {
-      const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=c9a0ca46550648b29ce125849232709&q=${location}&days=1&aqi=no&alerts=no&lang=vi`);
+      const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=c9a0ca46550648b29ce125849232709&q=${location}&days=5&aqi=no&alerts=no&lang=vi`);
       const data = await response.json();
       console.log(data);
       setWeather(data);
@@ -32,7 +69,7 @@ const WeatherComponent = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <div className={styles.timenow}>{weather?.location?.localtime?.slice(-5)}</div>
+        <div className={styles.timenow}>{currentTime.slice(0,5)}</div>
         <div className={styles.iconHeader}>
           <SignalHigh/>
           <Wifi/>
@@ -65,14 +102,13 @@ const WeatherComponent = () => {
       <div className={styles.footer}>
         <h1>Now</h1>
         <div className={styles.footerContainer}>
-          {weather?.forecast?.forecastday[0]?.hour?.slice(0, 4).map((item:any, index:number)=>(
+          {handleWeatherHourly().map((item:any, index:number)=>(
             <div className={styles.weatherItem}>
               <img src={item.condition.icon} alt="" />
               <div className={styles.temperatureNow}>{Math.floor(item.temp_c)}°</div>
               <div className={styles.timeNow}>{index === 0 ? 'Now' : item.time.slice(-5)}</div>
             </div>
           ))}
-          
         </div>
       </div>
     </div>
